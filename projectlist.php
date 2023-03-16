ff02aeeb1dd2637afd76a1518bd11f2458f709a0
jesse gauthier <jesse@gauthierstudios.ca>
2023-03-16 14:07:22 -0400

    .

--- projectlist.php
+++ projectlist.php
@@ -1,27 +1,18 @@
 <?php
+$conn = mysqli_connect('localhost:3306', 'project_manager', 'Bailey1967!!','project_tracker');
 
-// $servername = "localhost:3306";
-// $username = "project_manager";
-// $password = "Bailey1967!!";
-// $dbname = "project_tracker";
-
-// Create connection
-$con = mysqli_connect('localhost:3306', 'project_manager', 'Bailey1967!!', 'project_tracker');
-// Check connection
-if ($con->connect_error) {
+if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT projectname FROM project_manager";
+$sql = "SELECT * FROM project_manager";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "Project" . $row["projectname"];
+    echo "id: " . $row["projectname"];
   }
 } else {
   echo "0 results";
 }
 $conn->close();
-?>
