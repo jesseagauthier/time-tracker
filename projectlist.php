97f20cecd8d567c93c9b1af87d30477604153118
jesse gauthier <jesse@gauthierstudios.ca>
2023-03-16 17:04:41 -0400

    .

--- projectlist.php
+++ projectlist.php
@@ -1,8 +1,6 @@
         <?php
 
         $conn = mysqli_connect('localhost:3306', 'project_manager', 'Bailey1967!!', 'project_tracker');
-
-
         $sql = "SELECT * FROM project_manager;";
         $result = mysqli_query($conn, $sql);
 
