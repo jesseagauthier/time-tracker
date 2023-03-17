1bf1bd3759de5476f2094e9cdd47f482f1c78633
jesse gauthier <jesse@gauthierstudios.ca>
2023-03-17 09:16:21 -0400

    .

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
                 while ($row = mysqli_fetch_assoc($result)) {
                     $project_name = $row['projectname'];
 
-                    echo "<ol>" . "<li>" . $project_name . "</li>" . "</ol>" . "<br>";
+                    echo "<ul>" . "<li>" . $project_name . "</li>" . "</ul>" . "<br>";
                 }
 
 
