57e85a088c899f88bfd82e0f83f7e6b55a63b4ad
jesse gauthier <jesse@gauthierstudios.ca>
2023-03-16 20:51:08 -0400

    ..

--- index.php
+++ index.php
@@ -34,26 +34,26 @@
                     </select>
                 </form>
             </div>
-            <?php
-
-                            $conn = mysqli_connect('localhost:3306', 'project_manager', 'Bailey1967!!', 'project_tracker');
-                            $sql = "SELECT * FROM project_manager;";
-                            $result = mysqli_query($conn, $sql);
-
-
-                            echo "<h2>Project List</h2>". "<br>";
-
-
-                            while ($row = mysqli_fetch_assoc($result)) {
-                              echo $row['projectname'] . "<br>";
-                            }
-
-
-                            $conn->close();
-                            ?>
         </div>
     </div>
 </main>
+        <?php
+
+                $conn = mysqli_connect('localhost:3306', 'project_manager', 'Bailey1967!!', 'project_tracker');
+                $sql = "SELECT * FROM project_manager;";
+                $result = mysqli_query($conn, $sql);
+
+
+                echo "<h2>Project List</h2>". "<br>";
+
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                  echo $row['projectname'] . "<br>";
+                }
+
+
+                $conn->close();
+                ?>
 <!-- <footer><p>Version .02</p></footer> -->
 
 <body>
