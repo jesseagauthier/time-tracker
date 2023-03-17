2edce7140ac14dd3b17393c7d319db38550c9c53
jesse gauthier <jesse@gauthierstudios.ca>
2023-03-17 09:30:28 -0400

    adds reset

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="reset.css">
     <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="style.php" media="screen">
     <script src="main.js" defer></script>
@@ -49,7 +50,9 @@
             </div>
         </div>
 </main>
-<footer><p>Version .4</p></footer>
+<footer>
+    <p>Version .4</p>
+</footer>
 
 <body>
 </body>
