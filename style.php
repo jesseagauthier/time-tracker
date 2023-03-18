a14184dd6633c58cf5010056cefbd9323139cce4
jesse gauthier <jesse@gauthierstudios.ca>
2023-03-18 11:45:48 -0400

    d

--- style.php
+++ style.php
@@ -5,3 +5,35 @@
 header("Content-type: text/css");
 
 ?>
+
+
+#submit {
+all: unset;
+background-color: black;
+color: white;
+padding: .50em;
+cursor: pointer;
+}
+
+
+
+#submit:hover {
+background-color: white;
+color: black;
+}
+
+.project-summary {
+width: 100%;
+}
+
+
+.project-summary thead tr {
+width: fit-content;
+}
+
+.project-summary th {
+font-weight: bold;
+font-size: 1rem;
+margin: 0 4em;
+
+}
