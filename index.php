e7f201d88bbb676fea9922c1777ade7415723909
jesse gauthier <jesse@gauthierstudios.ca>
2023-03-18 10:01:59 -0400

    d

--- index.php
+++ index.php
@@ -29,7 +29,6 @@
             <div class="row">
                 <!-- Input -->
                 <div class="col-11 offset-1 text-center bg-primary m-1">
-                    <h3 class="text-white my-3 align-self-center">Add A Project</h3>
                     <form class="my-4 align-self-center" method="post" action="newproject.php">
                         <input type="text" id="project_name" name="project-name" required>
                         <input type="submit" name="Submit" id="Submit" value="New Project">
