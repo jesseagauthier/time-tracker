ab6f3b3c7ae25d17391b67b1b6fa1419b6c437e3
jesse gauthier <jesse@gauthierstudios.ca>
2023-03-18 13:12:04 -0400

    .

--- newproject.php
+++ newproject.php
@@ -1,42 +1,34 @@
-<?php
-// database connection code
-$con = mysqli_connect('localhost:3306', 'project_manager', 'Bailey1967!!','project_tracker');
+// Get the form data
+$projectname = $_POST['project-name'];
+$contact_name = $_POST['contact'];
+$contact_email = $_POST['email'];
+$contact_phone = $_POST['phone'];
+$project_type = $_POST['project_type'];
 
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
+// Escape special characters to prevent SQL injection
+$projectname = mysqli_real_escape_string($con, $projectname);
+$contact_name = mysqli_real_escape_string($con, $contact_name);
+$contact_email = mysqli_real_escape_string($con, $contact_email);
+$contact_phone = mysqli_real_escape_string($con, $contact_phone);
+$project_type = mysqli_real_escape_string($con, $project_type);
+
+// Check if the project already exists in the database
+$sql = "SELECT * FROM `project_manager` WHERE `projectname` = '$projectname' AND `contact_email` = '$contact_email'";
+$result = mysqli_query($con, $sql);
+
+if(mysqli_num_rows($result) > 0) {
+// If the project already exists, prevent the submission
+echo "This project already exists in the database.";
+} else {
+// If the project doesn't exist, insert it into the database
+$insert_sql = "INSERT INTO `project_manager` (`projectname`, `contact_name`, `contact_email`, `contact_phone`, `project_type`)
+VALUES ('$projectname', '$contact_name', '$contact_email', '$contact_phone', '$project_type')";
+
+$insert_result = mysqli_query($con, $insert_sql);
+
+if($insert_result) {
+echo "Project added successfully.";
+} else {
+echo "Error adding project: " . mysqli_error($con);
 }
-
-// Check if the form is submitted
-if(isset($_POST['Submit'])) {
-    // Get the form data
-    $projectname = $_POST['project-name'];
-    $contact_name = $_POST['contact'];
-    $contact_email = $_POST['email'];
-    $contact_phone = $_POST['phone'];
-    $project_type = $_POST['project_type'];
-
-    // Escape special characters to prevent SQL injection
-    $projectname = mysqli_real_escape_string($con, $projectname);
-    $contact_name = mysqli_real_escape_string($con, $contact_name);
-    $contact_email = mysqli_real_escape_string($con, $contact_email);
-    $contact_phone = mysqli_real_escape_string($con, $contact_phone);
-    $project_type = mysqli_real_escape_string($con, $project_type);
-
-    // database insert SQL code
-    $sql = "INSERT INTO `project_manager` (`projectname`, `contact_name`, `contact_email`, `contact_phone`, `project_type`)
-            VALUES ('$projectname', '$contact_name', '$contact_email', '$contact_phone', '$project_type')";
-
-    // insert in database
-    $rs = mysqli_query($con, $sql);
-
-    if($rs) {
-        // header("Refresh:0; url=index.php");
-        // echo "Project Added";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($con);
-    }
 }
-
-mysqli_close($con);
